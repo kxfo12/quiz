656f6ad78301b218401cc48de7eb48f027ab3b64
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-10-16 15:43:13 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,14 @@
-package com.example.test;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addQuestions();
+        addpod();
     }
 
-    public void wrong(View view) {
-        Toast.makeText(this, "Przemyśl swoją odpowiedź", Toast.LENGTH_SHORT).show();
+    ArrayList<String> questions = new ArrayList<String>();
+    ArrayList<String> podpowiedzi = new ArrayList<String>();
+    public int question = 0;
+    public TextView questionText = findViewById(R.id.questionText);
+
+    public int counterOfRightAnswers = 0;
+    public boolean rightOrWrong = false;
+    public Button podpo;
+    //podpo = findViewById(R.id.button4);
+
+    public void addQuestions(){
+        questions.add("Is Calcharo the leader of Ghost Hounds");
+        questions.add("Is Scar in Black Shores");
+        questions.add("Is The first instance one of Rover names");
+    }
+    public void addpod(){
+        questions.add("Who else?");
+        questions.add("What about Fractsidus");
+        questions.add("There was 3 names");
     }
 
-    public void wrongbut(View view) {
-        Toast.makeText(this, "Przemyśl swoją odpowiedź ale nie jest najgożej", Toast.LENGTH_SHORT).show();
+    public void yes(View view) {
+        rightOrWrong = true;
     }
 
-    public void correct(View view) {
-        Toast.makeText(this, "Witaj w klubie", Toast.LENGTH_SHORT).show();
+    public void no(View view) {
+        rightOrWrong = false;
+    }
+
+    public void next(View view) {
+        if(rightOrWrong == true){
+            counterOfRightAnswers += 1;
+        }
+        question += 1;
+        if(question > 3){
+            questionText.setText("Koniec quizu, dobre odpowiedzi: "+counterOfRightAnswers);
+        }
+        else{
+            questionText.setText(questions.get(question));
+        }
     }
 }
